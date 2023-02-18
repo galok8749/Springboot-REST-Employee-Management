c9c7f2489f4301240aa8e61472a6e2987b1113d7
galok8749 <galok8749@gmailcom>
2023-02-18 21:19:57 +0530

    return object in get request

--- src/main/java/com/alok/springbootdemo/controller/HomeController.java
+++ src/main/java/com/alok/springbootdemo/controller/HomeController.java
@@ -1,14 +1,24 @@
 package com.alok.springbootdemo.controller;
 
+import com.alok.springbootdemo.model.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HomeController {
     @RequestMapping("/")
     public String home() {
         return "Hello There";
+    }
+
+//    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    @GetMapping("/user")
+    public User user() {
+        User user = new User();
+        user.setId("1");
+        user.setName("alok");
+        user.setEmailId("alok@gmail.com");
+
+        return user;
     }
 }
