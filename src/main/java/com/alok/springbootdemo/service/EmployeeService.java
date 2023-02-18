54123fe40e531902f016dbcd3a9e0d146c6ab978
galok8749 <galok8749@gmailcom>
2023-02-18 22:30:51 +0530

    employee POST implementation

---
+++ src/main/java/com/alok/springbootdemo/service/EmployeeService.java
@@ -0,0 +1,8 @@
+package com.alok.springbootdemo.service;
+
+import com.alok.springbootdemo.model.Employee;
+
+public interface EmployeeService {
+
+    public Employee save(Employee employee);
+}
---
+++ src/main/java/com/alok/springbootdemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,22 @@
+package com.alok.springbootdemo.service;
+
+import com.alok.springbootdemo.controller.EmployeeController;
+import com.alok.springbootdemo.model.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+    List<Employee> employees = new ArrayList<>();
+    @Override
+    public Employee save(Employee employee) {
+        if(employee.getEmployeeId() == null || employee.getEmailId().isEmpty()) {
+            employee.setEmployeeId(UUID.randomUUID().toString());
+        }
+        employees.add(employee);
+        return employee;
+    }
+}
