73eedd0814aac17ee9e061e6c40e5d11d785f38a
galok8749 <galok8749@gmailcom>
2023-02-19 11:12:02 +0530

    H2 DB

---
+++ src/main/java/com/alok/springbootdemo/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.alok.springbootdemo.repository;
+
+import com.alok.springbootdemo.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, String> {
+
+}
